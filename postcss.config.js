27264b883b078b165f4beb68ac5941c33371d28d
Yuvraj Singh <yuvrajsingh808@gmail.com>
2021-02-03 10:58:00 +0530

    Bugs fixed

--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
 
-class TailwindExtractor {
-    static extract(content) {
-        return content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-    }
-}
-
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
