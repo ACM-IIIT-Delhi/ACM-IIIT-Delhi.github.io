dc04644750c5ac5ea2205e412eb9a203ffcf5503
Yuvraj Singh <yuvrajsingh808@gmail.com>
2021-02-09 16:27:02 +0530

    Alt homepage created

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,7 @@ module.exports = {
             preset: 'default',
         }),
         purgecss({
-            content: ['*.html'],
+            content: ['*.html', 'src/*.html'],
             defaultExtractor: content => content.match(/[\w-:/]+(?<!:)/g) || []
         }),
         autoprefixer
